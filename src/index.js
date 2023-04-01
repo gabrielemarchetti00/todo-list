a03ea8d3ddc3d69002b86a431ace979d89c0736b
gabrielemarchetti00 <gabriele.marchetti26@gmail.com>
2023-04-01 11:16:58 +0200

    add showDetails function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-function Todo(title, description, dueDate, priority, project) {
+function Todo(title, description, dueDate, priority, project, pos) {
   this.title = title;
   this.description = description;
   this.dueDate = dueDate;
   this.priority = priority;
   this.project = project;
+  this.pos = pos;
 }
 
 function showTodos(project) {
@@ -11,8 +12,57 @@ function showTodos(project) {
   middleBody.innerHTML = '';
   for (const i in project) {
     const todoDiv = document.createElement("div");
+    todoDiv.className = 'todos';
+    todoDiv.id = project[i].pos;
     todoDiv.textContent = `${project[i].title} - ${project[i].dueDate}`;
     middleBody.appendChild(todoDiv);
+  }
+  makeTodosClickable();
+}
+
+function makeTodosClickable() {
+  const todosDiv = document.querySelectorAll('.todos');
+  todosDiv.forEach((todo) => {
+  todo.addEventListener('click', () => {
+    for(const i in defaultProject){
+      if(todo.id == defaultProject[i].pos){
+        showDetails(defaultProject[i]);
+      }
+    }
+  });
+});
+}
+
+function showDetails(todo){
+  const detailsSection = document.querySelector('.details');
+  detailsSection.innerHTML = '';
+  const keys = Object.keys(todo);
+  for (let i = 0; i < keys.length - 1; i++) {
+    const detailDiv = document.createElement('div');
+    detailDiv.classList = 'single-detail';
+    const titleDiv = document.createElement('div');
+    const contentDiv = document.createElement("div");
+    switch(i){
+      case 0: 
+        titleDiv.textContent = 'Title:'
+        contentDiv.textContent = todo[keys[i]];
+        break;
+      case 1:
+        titleDiv.textContent = 'Description:'
+        contentDiv.textContent = todo[keys[i]];
+        break;
+      case 2:
+        titleDiv.textContent = 'Due date:'
+        contentDiv.textContent = todo[keys[i]];
+        break;
+      case 3:
+        titleDiv.textContent = 'Priority:'
+        contentDiv.textContent = todo[keys[i]];
+        break;
+    }
+    detailDiv.appendChild(titleDiv);
+    detailDiv.appendChild(contentDiv);
+    detailsSection.appendChild(detailDiv);
   }
 }
 
@@ -21,14 +71,17 @@ const uniProject = [];
 const sportProject = [];
 const holidayProject = [];
 
-defaultProject.push(new Todo("exam", "algorithms", "21/4", "high", "university"));
-uniProject.push(new Todo("exam", "algorithms", "21/4", "high", "university"));
+todoOne = new Todo("exam", "algorithms", "21/4", "high", "university", 1);
+defaultProject.push(todoOne);
+uniProject.push(todoOne);
 
-defaultProject.push(new Todo("basketball", "3v3, outdoor", "12/4", "low", "sport"));
-sportProject.push(new Todo("basketball", "3v3, outdoor", "12/4", "low", "sport"));
+todoTwo = new Todo("basketball", "3v3, outdoor", "12/4", "low", "sport", 2)
+defaultProject.push(todoTwo);
+sportProject.push(todoTwo);
 
-defaultProject.push(new Todo("sardegna", "santa teresa, one week", "13/9", "middle", "holiday"));
-holidayProject.push(new Todo("sardegna", "santa teresa, one week", "13/9", "middle", "holiday"));
+todoThree = new Todo("sardegna", "santa teresa, one week", "13/9", "middle", "holiday", 3);
+defaultProject.push(todoThree);
+holidayProject.push(todoThree);
 
 showTodos(defaultProject);
 
@@ -51,3 +104,10 @@ const allButton = document.querySelector('.all');
 allButton.addEventListener('click', () => {
   showTodos(defaultProject);
 });
+
+const deleteSection = document.querySelector('.delete');
+const deleteBtn = document.createElement('button');
+deleteBtn.addEventListener('click', () => {
+
+});
+
